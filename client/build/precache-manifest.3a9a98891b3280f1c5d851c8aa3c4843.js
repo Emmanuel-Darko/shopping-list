353e649ec884c00ffaf736458e3fafe59cd3b352
Emmanuel Darko <darkoemmanuelofosu@gmail.com>
2020-05-24 15:35:56 +0000

    added build folder

---
+++ client/build/precache-manifest.3a9a98891b3280f1c5d851c8aa3c4843.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c23f3ff2046a5d2c3383276023ed50c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "831b5baf169f4b611a4e",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ce8ca21f5f88431396f3",
+    "url": "/static/css/main.87cb4666.chunk.css"
+  },
+  {
+    "revision": "831b5baf169f4b611a4e",
+    "url": "/static/js/2.c65ea343.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c65ea343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8ca21f5f88431396f3",
+    "url": "/static/js/main.b5d0a7bc.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
